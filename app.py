91d7a3d7378db030cfaa1daea6d1cbbe24dcc7e8
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-06-27 17:09:53 +0200

    adding thanks template

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import redirect
+
 from data_processing import DataProcessing
 from map_of_the_world import CreatingMap
 
@@ -15,11 +17,19 @@ def index():
 def graph_diff(id):
     return render_template("chat.html")
 
-@app.route("/add-meetup")
+
+@app.route("/add-meetup", methods=['GET', 'POST'])
 def add_meetup():
-    towns = DataProcessing().get_all_towns()
-    return render_template("add-meetup.html", towns=towns)
+    if request.method == "GET":
+        towns = DataProcessing().get_all_towns()
+        return render_template("add-meetup.html", towns=towns)
+    if request.method == "POST":
+        return render_template("thanks.html")
+
+@app.route("/thanks")
+def thanks():
+    return "TAHNKS"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
